b63bf923d30d1b75ebb2e4b64d65977f095e138c
Strokin Constantine <strokinconstantine@gmail.com>
2025-01-06 22:36:16 +0300

    addition: zhirkov_low_level_programming book notes

--- notes/books/zhirkov_low_level_programming/chapter_2_assembly_language/hello_world.asm
+++ notes/books/zhirkov_low_level_programming/chapter_2_assembly_language/hello_world.asm
@@ -4,7 +4,7 @@ global _START; not the same label as _start, cause labels are case sensitive
 
 section .data; section for data, characters are encoded in ASCII, code of symbol for new line is 10
 
-message: db 'hello, world!', 10; db directive is used to create byte data ( dw, dd, dq )
+message: db 'hello, world!', 10; db ( bytes ) directive is used to create byte data ( dw = words, dd = double words, dq = quad words )
 example1: db 5, 15, 1
 example2: times 20 db 42; times n cmd is a directive to repeat cmd n times in program code
 example3: dw 999
@@ -15,12 +15,18 @@ section .text; section for code
 
 _start:
 ; mov a, b means write value from register or memory or imm value 'b' into register or memory 'a', but mov cannot copy data from mem to mem and src and dst operands must be of the same size
-	mov rax, 1; rax holding system call's number
+	mov rax, 1; rax holding system call's number. rdi, rsi, rdx, r10, r8 and r9 are used to hold arguments
 	mov rdi, 1; argument 1, file descriptor ( where to write )
 	mov rsi, message; argument 2, where does the string start? 
 	mov rdx, 14; argument 3, how many bytes to write?
-	syscall; system call in *nix systems
+	syscall; system call in *nix systems, changes rcx and r11
 	
 	mov rax, 60; exit syscall number
 	xor rdi, rdi; effective zeroing
 	SYSCALL; assembly language is case insensitive
+	
+; to compile and execute:
+; nasm -felf64 hello.asm -o hello.o
+; ld -o hello hello.o
+; chmod u+x hello
+; ./hello
---
+++ notes/books/zhirkov_low_level_programming/chapter_2_assembly_language/print_rax_value_in_hexadecimal_format.asm
@@ -0,0 +1,40 @@
+section .data
+codes:
+	db	'0123456789ABCDEF'
+
+section .text
+global _start
+_start:; global label
+	mov rax, 0x11223344556677df; number to be printed
+	
+	mov rdi, 1; descriptor of stdout
+	mov rdx, 1; number of bytes to write
+	mov rcx, 64; number is 64 bit long, loop will iterate 16 ( = 64 / 4 ) times
+
+.loop:; local label with full name _start.loop ( because _start is the last used dotless global label ) 
+	push rax; saves value of rax onto the stack
+	sub rcx, 4; prepares to shift the number to isolate the next 4 bits
+
+	; rax -- eax -- ax -- ah + al
+	; rcx -- ecx -- cx -- ch + cl
+
+	sar rax, cl; this instruction shifts the required hexadecimal digit into the least significant 4 bits of rax
+	and rax, 0xf; masks the lowest 4 bits of rax to extract a single hexadecimal digit
+	
+	
+	; square brackets denote indirect addressing, the address is written inside them
+	; lea = load effective address = calculate an address of a memory cell and store it
+	lea rsi, [codes + rax]; calculates the address of the character in codes corresponing to extracted digit
+	
+	mov rax, 1; system call number for write
+	push rcx; save rcx because syscall can modify it
+	syscall
+	pop rcx
+
+	pop rax
+	test rcx, rcx; checks if rcx is zero by performing a bitwise AND
+	jnz .loop; if rcx is not zero, jumps back to .loop
+
+	mov rax, 60
+	xor rdi, rdi
+	syscall
