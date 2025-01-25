0e854f1b9d8581d4b9a521920f7ae32e0b310f7e
Strokin Constantine <strokinconstantine@gmail.com>
2025-01-25 21:37:14 +0300

    addition: notes on C++ lecture 2 from meshcherin's course

---
+++ notes/lectures/programming/meshcherin_cpp/lecture_2_terminal_commands_and_program_compilation/notes.cpp
@@ -0,0 +1,24 @@
+/*
+For every literal hardcoded its type
+
+5; // int
+0.2; // double
+"abc"; // const char*
+'a'; // char
+
+Literal suffixes:
+
+	5u; // unsigned int
+	
+	overflow of int -> UB
+	
+	overflow of unsigned int -> OKAY
+	
+	0.2f; // float
+
+
+
+*/
+
+
+
