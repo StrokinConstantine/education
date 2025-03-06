3d18ee21d9d9fdeca4a5abecd95cc92fb2a6da3d
Strokin Constantine <strokinconstantine@gmail.com>
2025-03-06 23:08:38 +0300

    addition: notes on lecture 2 from algorithms and data structures book

---
+++ notes/books/babichev_lectures_on_algorithms_and_data_structures/lecture_2/bubble_sort.cpp
@@ -0,0 +1,2 @@
+void bubblesort(int *a, int n) { bool sorted = false; while (!sorted) { sorted = true; for (int i = 0; i < n-1; i++) { if (a[i] > a[i+1]) { int tmp = a[i]; a[i] = a[i+1]; a[i+1] = tmp; sorted = false;
+} } } }
