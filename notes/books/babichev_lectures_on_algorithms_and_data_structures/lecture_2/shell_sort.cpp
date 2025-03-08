f5854e74ff67fb377227574ce0747c85c2b7c92d
Strokin Constantine <strokinconstantine@gmail.com>
2025-03-08 20:53:58 +0300

    addition: notes on lecture 2 from algorithms and data structures book

---
+++ notes/books/babichev_lectures_on_algorithms_and_data_structures/lecture_2/shell_sort.cpp
@@ -0,0 +1,4 @@
+void shellsort(int *a, int n) { int h; for (h = 1; h <= n / 9; h = 3*h + 1) ;
+for ( ; h > 0; h /= 3) { for (int i = h; i < n; i++) {
+	void shellsort(int *a, int n) { int h; for (h = 1; h <= n / 9; h = 3*h + 1) ;
+for ( ; h > 0; h /= 3) { for (int i = h; i < n; i++) {
